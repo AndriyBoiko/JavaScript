5c0599bd540db987e6054bdafa009f901243c751
Javas9 <talisman.andriy@gmail.com>
2022-02-02 08:37:02 +0200

    update code add comments

--- lesson1/classWork/js.js
+++ lesson1/classWork/js.js
@@ -1,81 +1,83 @@
-// // console.log(Math.floor(Math.random()*10))
-// let arrNumb = [2, 15, 10, -23, 21, -7, 12, 25, 21, -71]
-// result = arrNumb.reduce(function(a, b) {
-//     return a + b;
-// });
-// console.log(result)
-// // console.log(arrNumb.length)
-
-let book = {
-    bookName: "Чорний Ворон",
-    bookPages: 431,
-    bookGen: "Історичний"
-};
-// console.log(book);
-let book1 = {
-    bookName: 'Характерник',
-    bookGen: 'Бойовик',
-    pages: 321,
-    author: 'Автор'
-};
-// console.log(book1);
-
-let books = [
-    {
+// Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+console.log(Math.floor(Math.random() * 10)) // виводить в консоль рандомне число
+let arr = [2, 15, 10, -23, 21, -7, 12, 25, 21, -71] // створив масив з 10 чисел
+result = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9] // склав результат цих чисел в змінну
+console.log(result) // вивів змінну
+// console.log(arr.length)
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+{
+    let book = {
+        bookName: "Чорний Ворон",
+        bookPages: 431,
+        bookGen: "Історичний"
+    };
+    console.log(book);
+}
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+{
+    let book = {
         bookName: 'Характерник',
         bookGen: 'Бойовик',
-        bookPages: 431,
+        pages: 321,
+        author: 'Автор'
+    };
+    console.log(book);
+}
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори.
+let book = [
+    {
+        name: 'Характерник',
+        gen: 'Бойовик',
+        pages: 431,
         author: 'Author 1'
     },
     {
-        bookName: "Чорний Ворон",
-        bookPages: 431,
-        bookGen: "Історичний",
+        name: "Чорний Ворон",
+        pages: 431,
+        gen: "Історичний",
         author: 'Автор'
     },
     {
-        bookName: "Book Red",
-        bookPages: 131,
-        bookGen: "Adventure",
+        name: "Book Red",
+        pages: 131,
+        gen: "Adventure",
         author: 'Автор Bok'
     },
     {
-        bookName: "Family Red",
-        bookPages: 131,
-        bookGen: "Family",
+        name: "Family Red",
+        pages: 131,
+        gen: "Family",
         author: 'Автор Bok'
     },
 
 ]
-console.log(books[0], books[1])
-// console.log(books[1])
-console.log(books[2])
-console.log(books[3])
-
+// Вивести кожну книгу як окремий об'єкт
+console.log(book[0], book[1])
+// console.log(book[1])
+console.log(book[2])
+console.log(book[3])
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
 let height = 23;
 let width = 10;
-s = height*width
+s = height * width
 console.log(`Площа прмокутника (${s})cm`)
 
 // V = π R2 h
-//
-
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
 let heightC = 10;
 let dC = 4;
 let radius = dC / 2;
-console.log(radius);
 let r2 = Math.pow(radius, 2);
 const PI = 3.14;
 let V = PI * r2 * heightC;
 console.log(V);
 
-
-//_-----------------------
-
 // У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
 // Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
 let n = 3
 let m = 4
-k = Math.sqrt(Math.pow(n, 2)+ Math.pow(m, 2));
+k = Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
 console.log(k)
 
--- lesson1/homeWork/js.js
+++ lesson1/homeWork/js.js
@@ -1,62 +1,63 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+  let hello = "hello";
+  let owu = "owu";
+  let com = "com";
+  let ua = "ua";
+  let one = 1;
+  let ten = 10;
+  let n999 = -999;
+  let n123 = 123;
+  let pi = 3.14;
+  let n27 = 2.7;
+  let n16 = 16;
+  let bullTru = true;
+  let bullFalse = false;
+//     Вивести кожну змінну за допомогою: console.log
+  console.log(hello);
+  console.log(owu);
+  console.log(com);
+  console.log(ua);
+  console.log(one);
+  console.log(ten);
+  console.log(n999);
+  console.log(n123);
+  console.log(pi);
+  console.log(n27);
+  console.log(n16);
+  console.log(bullTru);
+  console.log(bullFalse);
 
-let hello = "hello";
-let owu = "owu";
-let com = "com";
-let ua = "ua";
-let one = 1;
-let ten = 10;
-let n999 = -999;
-let n123 = 123;
-let pi = 3.14;
-let n27 = 2.7;
-let n16 = 16;
-let bullTru = true;
-let bullFalse = false;
+//Вивести кожну змінну за допомогою: document.write
+  document.write(`<p>${hello}</p>`);
+  document.write(`<p>${owu}</p>`);
+  document.write(`<p>${com}</p>`);
+  document.write(`<p>${ua}</p>`);
+  document.write(`<p>${one}</p>`);
+  document.write(`<p>${ten}</p>`);
+  document.write(`<p>${n999}</p>`);
+  document.write(`<p>${n123}</p>`);
+  document.write(`<p>${pi}</p>`);
+  document.write(`<p>${n27}</p>`);
+  document.write(`<p>${n16}</p>`);
+  document.write(`<p>${bullTru}</p>`);
+  document.write(`<p>${bullFalse}</p>`);
 
-console.log(hello);
-console.log(owu)
-console.log(com)
-console.log(ua)
-console.log(one)
-console.log(ten)
-console.log(n999)
-console.log(n123)
-console.log(pi)
-console.log(n27)
-console.log(n16)
-console.log(bullTru)
-console.log(bullFalse)
-
-
-document.write(`<p>${hello}</p>`);
-document.write(`<p>${owu}</p>`);
-document.write(`<p>${com}</p>`);
-document.write(`<p>${ua}</p>`);
-document.write(`<p>${one}</p>`);
-document.write(`<p>${ten}</p>`);
-document.write(`<p>${n999}</p>`);
-document.write(`<p>${n123}</p>`);
-document.write(`<p>${pi}</p>`);
-document.write(`<p>${n27}</p>`);
-document.write(`<p>${n16}</p>`);
-document.write(`<p>${bullTru}</p>`);
-document.write(`<p>${bullFalse}</p>`);
-
-
-alert(hello)
-alert(owu)
-alert(com)
-alert(ua)
-alert(one)
-alert(ten)
-alert(n999)
-alert(n123)
-alert(pi)
-alert(n27)
-alert(n16)
-alert(bullTru)
-alert(bullFalse)
-
+// Вивести кожну змінну за допомогою:  alert,
+  alert(hello)
+  alert(owu)
+  alert(com)
+  alert(ua)
+  alert(one)
+  alert(ten)
+  alert(n999)
+  alert(n123)
+  alert(pi)
+  alert(n27)
+  alert(n16)
+  alert(bullTru)
+  alert(bullFalse)
+//Переприсвоїd кожній змінній з завдання значення на довільне.
 hello = "hello world";
 owu = "owu-cenobi";
 com = "net";
@@ -70,6 +71,8 @@ n27 = 227;
 n16 = 1623;
 bullTru = "2";
 bullFalse = 0;
+
+//Вивести кожну змінну за допомогою: document.write
 
 document.write(`<div>${hello}</div>`);
 document.write(`<div>${owu}</div>`);
@@ -85,6 +88,8 @@ document.write(`<div>${n16}</div>`);
 document.write(`<div>${bullTru}</div>`);
 document.write(`<div>${bullFalse}</div>`);
 
+
+//     Вивести кожну змінну за допомогою: console.log
 console.log(hello);
 console.log(owu);
 console.log(com);
@@ -99,6 +104,8 @@ console.log(n16);
 console.log(bullTru);
 console.log(bullFalse);
 
+
+// Вивести кожну змінну за допомогою:  alert,
 alert(hello);
 alert(owu);
 alert(com);
@@ -113,43 +120,48 @@ alert(n16);
 alert(bullTru);
 alert(bullFalse);
 
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
 let firstName1 = "Andriy";
 let middleName1 = " Boiko ";
 let lastName1 = "Volodymyrovych";
 
+//З'єднаd їх в одну змінну person. (Не об'єкт)
 let person = firstName1 + middleName1 + lastName1;
 console.log(person);
 
 
-let firstName = prompt('як тебе звати');
+// Pа допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+let firstName = prompt('Як тебе звати');
 let lastName = prompt('Як ти по-батькові');
 let age = prompt('Скільки тобі років?');
-
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
 alert(`Вітаю ${firstName}  ${lastName}  Тобі  ${age}`);
 
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 let a = 100;
 let b = '100';
 let c = true;
-
 console.log(typeof a);
 console.log(typeof b);
 console.log(typeof c);
+// Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+    // В однакових виразах не використовувати однакові оператори!!!
+console.log(5 < 6); // true
+console.log(5 > 6); //false
+console.log(5 >= 6);//false
+console.log(5 === 6);//false
+console.log(10 === 10);// true
+console.log(10 >= 10);// true
+console.log(10 > 10);//false
+ console.log(10 < 10);//false
+console.log(123 === '123');//false
+console.log(123 !== '123');// true
 
-console.log(5 < 6);
-console.log(5 > 6);
-console.log(5 >= 6);
-console.log(5 === 6);
-console.log(10 === 10);
-console.log(10 >= 10);
-console.log(10 > 10);
- console.log(10 < 10);
-console.log(123 === '123');
-console.log(123 == '123');
-
-
+//- Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
 let str = "20";
 let a1 = 5;
-document.write(str + a1 + "<br/>");
-document.write(str - a1 + "<br/>");
-document.write(str * "2" + "<br/>");
-document.write(str / 2 + "<br/>");
+document.write(str + a1 + "<br/>"); //205 просто до числа дописує строку
+document.write(str - a1 + "<br/>"); //15 магія розуміє шо не може від числа відняти строку бере собі рообить строку числом і віднімає
+document.write(str * "2" + "<br/>"); //40 бачить знак множення в строці бачить число і сам собі множить
+document.write(str / 2 + "<br/>"); //10 бачить знак ділення в строці бачить число і сам собі множить
